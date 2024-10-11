004e97b91c33a2c7f984f75fd592a6499447a6b7
waseem-medhat <wipdev.tech@gmail.com>
2024-10-11 21:42:22 +0300

    Add exercises and tests for "Basic Types"

--- basic_types/exercises.ex
+++ basic_types/exercises.ex
@@ -1,7 +1,85 @@
 defmodule Spirit.BasicTypes do
+  @moduledoc """
+  Exercises for "Basic Types"
+  Guide Page: <https://hexdocs.pm/elixir/basic-types.html>
+
+  Simple exercises for a simple guide, but they should get you used to basic
+  Elixir syntax and data structures.
+
+  """
+
+  @doc """
+  Returns a tuple with two elements:
+  - The atom `:ok`
+  - A list with the numbers 1 and 2
+
+  """
   def simple_tuple() do
   end
 
-  def to_percentage(proportion) do
+  @doc """
+  Converts the given proportion to a percentage, rounded to nearest integer,
+  and formats it as a string with the percent sign.
+
+  ## Examples
+
+      iex> Spirit.BasicTypes.format_percentage(0.5)
+      iex> "50%"
+
+      iex> Spirit.BasicTypes.format_percentage(-1.236)
+      iex> "-124%"
+
+  """
+  def format_percentage(proportion) do
+  end
+
+  @doc """
+  Checks if one argument is truthy and the other is falsy.
+
+  ## Examples
+
+      iex> Spirit.BasicTypes.opposites?(true, false)
+      true
+
+      iex> Spirit.BasicTypes.opposites?(nil, [])
+      true
+
+      iex> Spirit.BasicTypes.opposites?(1, "hi")
+      false
+
+  """
+  def opposites?(a, b) do
+  end
+
+  @doc """
+  Checks if one argument is `true` and other other is `false`.
+
+  Raises if non-boolean arguments are given.
+
+  ## Examples
+
+      iex> Spirit.BasicTypes.boolean_opposites?(true, false)
+      true
+
+      iex> Spirit.BasicTypes.boolean_opposites?(nil, [])
+      BadBooleanError
+
+      iex> Spirit.BasicTypes.boolean_opposites?(1, "hi")
+      BadBooleanError
+
+  """
+  def boolean_opposites?(a, b) do
+  end
+
+
+  @doc """
+  Converts the given string to uppercase and appends an exclamation mark at the
+  end.
+
+      iex> Spirit.BasicTypes.shout("hi")
+      "HI!"
+
+  """
+  def shout(string) do
   end
 end
--- basic_types/tests.exs
+++ basic_types/tests.exs
@@ -1,20 +1,43 @@
-defmodule BasicTypes.BasicTypesTest do
+defmodule Spirit.BasicTypesTest do
   use ExUnit.Case
-  doctest Spirit.BasicTypes
 
   test "simple_tuple/0" do
-    tuple = Spirit.BasicTypes.simple_tuple()
+    result = Spirit.BasicTypes.simple_tuple()
 
-    assert is_tuple(tuple)
-    assert tuple_size(tuple) == 2
-
-    {atom, string} = tuple
-
+    assert {atom, list} = result
     assert atom == :ok
-    assert string == "spirit"
+    assert list == [1, 2]
   end
 
-  test "to_percentage/1" do
-    assert is_integer(Spirit.BasicTypes.to_percentage(0.5))
+  test "format_percentage/1" do
+    result = Spirit.BasicTypes.format_percentage(0.5)
+
+    assert result == "50%"
+
+    result = Spirit.BasicTypes.format_percentage(-1.236)
+
+    assert result == "-124%"
+  end
+
+  test "opposites?/2" do
+    assert Spirit.BasicTypes.opposites?(true, false) == true
+    assert Spirit.BasicTypes.opposites?(nil, []) == true
+    assert Spirit.BasicTypes.opposites?(1, "hi") == false
+  end
+
+  test "boolean_opposites?/2" do
+    assert Spirit.BasicTypes.boolean_opposites?(true, false) == true
+
+    assert_raise BadBooleanError, fn ->
+      Spirit.BasicTypes.boolean_opposites?(nil, [])
+    end
+
+    assert_raise BadBooleanError, fn ->
+      Spirit.BasicTypes.boolean_opposites?(1, "hi")
+    end
+  end
+
+  test "shout/1" do
+    assert Spirit.BasicTypes.shout("hi") == "HI!"
   end
 end

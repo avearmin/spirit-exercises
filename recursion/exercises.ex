f7be292e978f584c46b93bdc47ce10182a4b484c
avearmin <arminradoncic1@gmail.com>
2025-02-04 16:37:51 -0500

    Add accumlators to exercises

--- recursion/exercises.ex
+++ recursion/exercises.ex
@@ -4,19 +4,6 @@ defmodule Spirit.Recursion do
   Guide Page: <https://hexdocs.pm/elixir/recursion.html>
 
   These exercises will get you used to using recursion as a replacement for loop constructs.
-
-  Note:
-    Many of these exercises can be can be completed using an accumulator or written without one. 
-    Implementing a solution without an accumulator is an extra challenge. If you choose to use 
-    an accumulator, create a helper function that performs the recursion and call it from your 
-    main function. For example, a sum function using an accumulator can be written as follows:
-
-      def sum(list) do
-        sum(list, 0)
-      end
-
-      defp sum([], acc), do: acc
-      defp sum([head | tail], acc), do: sum(tail, acc + head)
   """
 
   @doc """
@@ -31,20 +18,6 @@ defmodule Spirit.Recursion do
       nil
   """
   def last(list) do
-  end
-
-  @doc """
-  Returns a list of integers where each integer is squared.
-
-  ## Examples
-      
-      iex> Spirit.Recursion.square_each([2, 4, 6])
-      [4, 16, 36]
-      
-      iex> Spirit.Recursion.square_each([])
-      []
-  """
-  def square_each(list) do
   end
 
   @doc """
@@ -69,18 +42,46 @@ defmodule Spirit.Recursion do
 
   ## Examples
 
-      iex> Spirit.Recursion.average([1, 2, 3, 4, 5])
+      iex> Spirit.Recursion.average([1, 2, 3, 4, 5], 0, 0)
       3
       
-      iex> Spirit.Recursion.average([1])
+      iex> Spirit.Recursion.average([1], 0, 0)
       1
       
-      iex> Spirit.Recursion.average([])
+      iex> Spirit.Recursion.average([], 0, 0)
       0
   """
-  def average(list) do
+  def average(list, acc, count) do
   end
 
+  @doc """
+  Returns a reversed list.
+
+  ## Examples
+      
+      iex> Spirit.Recursion.reverse([1, 2, 3, 4, 5], [])
+      [5, 4, 3, 2, 1]
+
+      iex> Spirit.Recursion.reverse([], [])
+      []
+  """
+  def reverse(list, acc) do
+  end
+
+  @doc """
+  Returns a list of integers where each integer is squared.
+
+  ## Examples
+      
+      iex> Spirit.Recursion.square_each([2, 4, 6], [])
+      [4, 16, 36]
+      
+      iex> Spirit.Recursion.square_each([])
+      []
+  """
+  def square_each(list, acc) do
+  end
+ 
   @doc """
   Returns a list with each element set according to "fizzbuzz" rules.
 
@@ -93,30 +94,16 @@ defmodule Spirit.Recursion do
 
   ## Examples
       
-      iex> Spirit.Recursion.fizzbuzz([1, 2, 3, 4, 5])
+      iex> Spirit.Recursion.fizzbuzz([1, 2, 3, 4, 5], [])
       [1, 2, :fizz, 4, :buzz]
       
-      iex> Spirit.Recursion.fizzbuzz([-3, -5, -15, 0])
+      iex> Spirit.Recursion.fizzbuzz([-3, -5, -15, 0], [])
       [:fizz, :buzz, :fizzbuzz, :fizzbuzz]
       
-      iex> Spirit.Recursion.fizzbuzz([])
+      iex> Spirit.Recursion.fizzbuzz([], [])
       []
   """
-  def fizzbuzz(list) do
-  end
-
-  @doc """
-  Returns a reversed list.
-
-  ## Examples
-      
-      iex> Spirit.Recursion.reverse([1, 2, 3, 4, 5])
-      [5, 4, 3, 2, 1]
-
-      iex> Spirit.Recursion.reverse([])
-      []
-  """
-  def reverse(list) do
+  def fizzbuzz(list, acc) do
   end
   
   @doc """
@@ -125,16 +112,16 @@ defmodule Spirit.Recursion do
 
   ## Examples
 
-      iex> Spirit.Recursion.partial_sums([1, 2, 3, 4])
+      iex> Spirit.Recursion.partial_sums([1, 2, 3, 4], [], 0)
       [1, 3, 6, 10]
 
-      iex> Spirit.Recursion.partial_sums([1])
+      iex> Spirit.Recursion.partial_sums([1], [], 0)
       [1]
 
-      iex> Spirit.Recursion.partial_sums([])
+      iex> Spirit.Recursion.partial_sums([], [], 0)
       []
   """
-  def partial_sums(list) do
+  def partial_sums(list, acc, running_sum) do
   end
 
   @doc """
@@ -142,21 +129,21 @@ defmodule Spirit.Recursion do
 
   ## Examples
       
-      iex> Spirit.Recursion.merge([1, 3, 5], [2, 4, 6])
+      iex> Spirit.Recursion.merge([1, 3, 5], [2, 4, 6], [])
       [1, 2, 3, 4, 5, 6]
       
-      iex> Spirit.Recursion.merge([1, 3, 5], [2, 4, 6, 7])
+      iex> Spirit.Recursion.merge([1, 3, 5], [2, 4, 6, 7], [])
       [1, 2, 3, 4, 5, 6, 7]
 
-      iex> Spirit.Recursion.merge([1, 3, 5], [])
+      iex> Spirit.Recursion.merge([1, 3, 5], [], [])
       [1, 3, 5]
 
-      iex> Spirit.Recursion.merge([], [2, 4, 6])
+      iex> Spirit.Recursion.merge([], [2, 4, 6], [])
       [2, 4, 6]
 
-      iex> Spirit.Recursion.merge([], [])
+      iex> Spirit.Recursion.merge([], [], [])
       []
   """
-  def merge(list_1, list_2) do
+  def merge(list_1, list_2, acc) do
   end
 end
--- recursion/exercises_test.exs
+++ recursion/exercises_test.exs
@@ -9,35 +9,35 @@ defmodule Spirit.RecursionTest do
     test "last/1" do
       assert Recursion.last([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) == 0
     end
-
-    test "square_each/1" do
-      assert Recursion.square_each([5, 10, 15, 20]) == [25, 100, 225, 400]
-    end
-
+     
     test "sorted/1" do
       assert Recursion.sorted?([1, 2, 3, 13, 4]) == false
     end
 
-    test "average/1" do
-      assert Recursion.average([10, 11, 20, 35]) == 19 
+    test "average/3" do
+      assert Recursion.average([10, 11, 20, 35], 0, 0) == 19 
+    end
+    
+    test "reverse/2" do
+      assert Recursion.reverse([5, 4, 3, 2, 1, 0], []) == [0, 1, 2, 3, 4, 5]
     end
 
-    test "fizzbuzz/1" do
-      assert Recursion.fizzbuzz([1, 2, 3, 4, 5, 6, 7, 8, 9, 15]) == [
+    test "square_each/2" do
+      assert Recursion.square_each([5, 10, 15, 20], []) == [25, 100, 225, 400]
+    end
+
+    test "fizzbuzz/2" do
+      assert Recursion.fizzbuzz([1, 2, 3, 4, 5, 6, 7, 8, 9, 15], []) == [
         1, 2, :fizz, 4, :buzz, :fizz, 7, 8, :fizz, :fizzbuzz
       ]
     end
-
-    test "reverse/1" do
-      assert Recursion.reverse([5, 4, 3, 2, 1, 0]) == [0, 1, 2, 3, 4, 5]
+     
+    test "partial_sums/3" do
+      assert Recursion.partial_sums([1, 5, 10, 20], [], 0) == [1, 6, 16, 36]
     end
 
-    test "partial_sums/1" do
-      assert Recursion.partial_sums([1, 5, 10, 20]) == [1, 6, 16, 36]
-    end
-
-    test "merge/2" do
-      assert Recursion.fizzbuzz([1, 4, 10], [0, 4, 9, 11]) == [0, 1, 4, 4, 9, 10, 11]
+    test "merge/3" do
+      assert Recursion.fizzbuzz([1, 4, 10], [0, 4, 9, 11], []) == [0, 1, 4, 4, 9, 10, 11]
     end
   end
 end
